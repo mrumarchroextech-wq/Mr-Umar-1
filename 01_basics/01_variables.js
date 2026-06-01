1b4b22ba063c299c27b50b7a4cf3f38243a534d4
mrumarchroextech-wq <mrumarchroextech@gmail.com>
2026-06-01 06:14:05 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const AccountId = 11224433
+let AccountEmail = "mr-umar@gmail.com"
+var AccountPassword = "11225533"
+AccountCity = "Saharanpur"
+
+AccountEmail = "mr-umar13@gmail.com"
+AccountPassword = "113321"
+AccountCity = "Dehrandun"
+
+console.log(AccountId);
+console.table([AccountEmail,AccountPassword,AccountCity])
